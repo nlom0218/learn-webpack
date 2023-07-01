543d4bb6295fb53bbf80d1f9eb25a10c7f975805
nlom0218 <nlom0218@naver.com>
2023-07-01 22:51:52 +0900

    feat: Generating HTML Files Autometically During Webpack Build Process

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -50,5 +51,6 @@ module.exports = {
     new TerserPlugin(),
     new MiniCssExtractPlugin({ filename: 'styles.[contenthash].css' }),
     new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(),
   ],
 };
