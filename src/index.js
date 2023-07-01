3e6f232a0e540ebe309ab6c1a88dd857376b8169
nlom0218 <nlom0218@naver.com>
2023-07-01 18:04:08 +0900

    feat: Heading CSS With Webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import helloWorld from './hello-world';
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
 import addImage from './add-image';
 
-helloWorld();
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
+
 addImage();
