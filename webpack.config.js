d16eeb10c30fa2edb2053c0ecf6dc013afd72de8
nlom0218 <nlom0218@naver.com>
2023-07-01 18:12:20 +0900

    feat: Handing SASS

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,13 @@ module.exports = {
         test: /\.txt/,
         type: 'asset/source',
       },
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader'],
+      // },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
